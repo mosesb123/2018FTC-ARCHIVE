8ea9a40ad24d78ad8ab543d7128e4dbcd2fe4008
Jacob Khalili <jakekali@users.noreply.github.com>
2017-12-16 19:02:35 -0500

    Depreciated Old Code & Created moveSpeedBasic Class
    Created moveSpeedBasic Class as descripted in issue #4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Old Code/AutoRedClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Old Code/AutoRedClose.java
@@ -37,6 +37,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
+@Disable
 @Autonomous(name="Red Close", group="Pushbot")
 public class AutoRedClose extends LinearOpMode {
 

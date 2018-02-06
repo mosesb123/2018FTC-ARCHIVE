a9d3f9187c1d9754c19b4073a9c65c2f3186880d
JoeB <joseph_braha119@flatbush.org>
2018-02-05 22:43:14 -0500

    May have fixed "left_drive" error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
@@ -50,7 +50,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-//@Disabled
+@Disabled
 @Autonomous(name="AutoBlueTest", group="Pushbot")
 public class AutoOpMode extends LinearOpMode {
 
